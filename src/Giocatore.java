41e90bfcdd210c0bc272b11ea093c31d3cb53c55
longo.21132 <longo.alessandro.studente@itispaleocapa.it>
2024-10-07 13:00:13 +0200

    set methods, main and menu

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -13,9 +13,10 @@ public class Giocatore {
     }
 
     public Giocatore() {
-
+        setNome(nome);
+        setCapitano(capitano);
+        setGoal(goal);
     }
-
 
     public String getNome(){
         return nome;
@@ -29,4 +30,31 @@ public class Giocatore {
         return goal;
     }
 
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setCapitano(boolean capitano){
+        this.capitano = capitano;
+    }
+
+    public void setGoal(int goal){
+        this.goal = goal;
+    }
+
+    public static void AggiungiGiocatore(String[]arrayGiocatore, boolean[]arrayCapitano, int[]arrayGoal, String giocatore, boolean capitano, int goal, int count){
+        arrayGiocatore[count] = giocatore;
+        arrayCapitano[count] = capitano;
+        arrayGoal[count] = goal;
+    }
+    public static void VisualizzaRosa(String[]arrayGiocatore, String[]arrayCapitano, String[]arrayGoal){
+        for (int i = 0; i < arrayGiocatore.length; i++){
+            if (arrayGiocatore[i] == null){
+                break;
+            }
+            else {
+                System.out.println("Giocatore: " + arrayGiocatore[i]+ " Capitano:" + arrayCapitano[i] + " Goal:" + arrayGoal[i]);
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
@@ -5,8 +6,34 @@ public class Main {
         Giocatore g2 = new Giocatore();
         Giocatore g3 = new Giocatore("Kylian Mbappè", false, 300);
 
+        Scanner in = new Scanner(System.in);
+        boolean input = false;
+        String[] arrayGiocatore = new String[10];
+        boolean[] arrayCapitano = new boolean[10];
+        int[] arrayGoal = new int[10];
+        int count = 0;
+        do {
 
+            System.out.println("1- Aggiungere un giocatore alla squadra, controllando che non ci sia più di un capitano");
+            System.out.println("2- Visualizzare tutti i giocatori della squadra");
+            System.out.println("3- Modificare i dati di un giocatore a scelta");
+            System.out.println("4- Cancellare un giocatore dalla squadra ");
+            System.out.println("5- Visualizzare i giocatori che hanno realizzato più di 5 goal.");
+            System.out.println("6- Visualizzare il nome del capitano");
+            System.out.println("7- Assegnare il ruolo di capitano in modo casuale se non ancora presente");
+            System.out.println("0- Esci dal concessionario");
 
+            int scelta = in.nextInt();
+
+            switch (scelta) {
+                case 1:
+
+                case 0:
+                    input = true;
+                    System.out.println("Stai uscendo dal concessionario...");
+                    break;
+            }
+        } while (!input);
     }
 }
 
